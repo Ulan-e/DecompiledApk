ad31a56548a007223c91daede3668860be22f9bd
Ulan Erkinbaev <ulanbek.erkinbaev@optimabank.kg>
2025-05-24 17:58:31 +0600

    fix search

--- app/src/main/java/kz/sapasoft/emark/app/ui/projects/ProjectsFragment.kt
+++ app/src/main/java/kz/sapasoft/emark/app/ui/projects/ProjectsFragment.kt
@@ -1,9 +1,12 @@
 package kz.sapasoft.emark.app.ui.projects
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -72,6 +75,7 @@ class ProjectsFragment : DaggerFragmentExtended(), OnProjectClickListener {
     }
 
     private fun initView(view: View) {
+        search(view)
         val textView = view.findViewById(R.id.tv_toolbar) as TextView
         Intrinsics.checkExpressionValueIsNotNull(textView, "tv_toolbar")
         textView.text = getString(R.string.nav_item_main)
@@ -160,6 +164,23 @@ class ProjectsFragment : DaggerFragmentExtended(), OnProjectClickListener {
         view.findViewById<TextView>(R.id.tv_retry).setOnClickListener {
             setObservers()
         }
+    }
+
+    private fun search(view: View){
+        view.findViewById<EditText>(R.id.et_search).addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                println("terra beforeTextChanged ")
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                println("terra onTextChanged ")
+                val query = s.toString()
+                adapter.clear()
+                viewModel.getFilteredProjects(query)
+            }
+            override fun afterTextChanged(s: Editable?) {
+                println("terra afterTextChanged ")
+            }
+        })
     }
 
     override fun onProjectClick(projectModel: ProjectModel) {
